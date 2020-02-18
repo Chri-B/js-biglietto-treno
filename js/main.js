18ec48fcc03585ae944e5978bd192835bb5edc9c
Christian Beretta <beretta.christian2602@gmail.com>
2020-02-18 15:05:42 +0100

    inserimento attribute visible

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@
 var anniPasseggero = prompt('Inserisci la tua età');
 var km = prompt('Inserisci i km che vuoi percorrere');
 
-var datoAnni = isNaN(anniPasseggero);
-var datoKm = isNaN(km);
-
 var prezzoPienoBiglietto = km * 0.21; // calcolo il prezzo pieno del biglietto come da riga 3
 
-var prezzoBiglietto20 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 20 ) );
-var prezzoBiglietto40 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 40 ) );
+var prezzoBiglietto20 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 20 ) ); // esprimo il valore della variabile tramite calcolo dello sconto 20%
+var prezzoBiglietto40 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 40 ) ); // esprimo il valore della variabile tramite calcolo dello sconto 40%
 
-if ( anniPasseggero < 18 ) {
-    document.getElementById('costoFinale').innerHTML = prezzoBiglietto20.toFixed(2);
-} else if (anniPasseggero >= 65) {
-    document.getElementById('costoFinale').innerHTML = prezzoBiglietto40.toFixed(2);
-} else {
-    document.getElementById('costoFinale').innerHTML = prezzoPienoBiglietto.toFixed(2);
+// inizio if
+if ( anniPasseggero < 18 ) { // insieme dei passeggeri minorenni con sconto 20%
+    document.getElementById('costoFinale').innerHTML = prezzoBiglietto20.toFixed(2) + '€';
+} else if (anniPasseggero >= 65) { // insieme dei passeggeri over 65 (compreso)
+    document.getElementById('costoFinale').innerHTML = prezzoBiglietto40.toFixed(2) + '€';
+} else { // insieme dei passeggeri di età tra i 18 (compresi) e i 65anni (esclusi)
+    document.getElementById('costoFinale').innerHTML = prezzoPienoBiglietto.toFixed(2) + '€';
 }
+
+document.getElementById('comparsa').setAttribute('class','visible');
 
 console.log(prezzoPienoBiglietto);
 console.log(prezzoBiglietto20);
