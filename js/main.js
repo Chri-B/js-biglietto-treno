a8c7d8f36d344c2438c85d9a6899539dea69106a
Christian Beretta <beretta.christian2602@gmail.com>
2020-02-18 14:51:35 +0100

    inserimento funzione per 2 cifre decimali

--- js/main.js
+++ js/main.js
@@ -7,17 +7,20 @@
 var anniPasseggero = prompt('Inserisci la tua età');
 var km = prompt('Inserisci i km che vuoi percorrere');
 
+var datoAnni = isNaN(anniPasseggero);
+var datoKm = isNaN(km);
+
 var prezzoPienoBiglietto = km * 0.21; // calcolo il prezzo pieno del biglietto come da riga 3
 
 var prezzoBiglietto20 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 20 ) );
 var prezzoBiglietto40 = ( prezzoPienoBiglietto - ( (prezzoPienoBiglietto / 100) * 40 ) );
 
 if ( anniPasseggero < 18 ) {
-    document.getElementById('costoFinale').innerHTML = prezzoBiglietto20;
-} else if (anniPasseggero > 65) {
-    document.getElementById('costoFinale').innerHTML = prezzoBiglietto40;
+    document.getElementById('costoFinale').innerHTML = prezzoBiglietto20.toFixed(2);
+} else if (anniPasseggero >= 65) {
+    document.getElementById('costoFinale').innerHTML = prezzoBiglietto40.toFixed(2);
 } else {
-    document.getElementById('costoFinale').innerHTML = prezzoPienoBiglietto;
+    document.getElementById('costoFinale').innerHTML = prezzoPienoBiglietto.toFixed(2);
 }
 
 console.log(prezzoPienoBiglietto);
